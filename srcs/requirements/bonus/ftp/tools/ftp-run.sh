859c3f19c6d0e19faf908a9caab36dc36f96054c
cteoh <teohcheeming@gmail.com>
2024-12-08 03:18:13 +0800

    refactor: Change SFTP server to normal FTP server
    Change from using SSH to perform file transfer (SFTP) to using normal
    FTP with vsftpd.

--- srcs/requirements/bonus/ftp/tools/ftp-run.sh
+++ srcs/requirements/bonus/ftp/tools/ftp-run.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-if [ -z "$(getent passwd | grep $SFTP_USER)" ]; then
-	adduser -D -G ftp -g ftp -h /var/www/html/ $SFTP_USER
-	SFTP_PASSWORD=$(base64 /run/secrets/ftp_password)
-	echo "$SFTP_USER:$SFTP_PASSWORD" | chpasswd
-	mkdir -p /var/www/html/.ssh/
-	chmod 700 /var/www/html/.ssh/
-	cat /run/secrets/ssh_key > /var/www/html/.ssh/authorized_keys
-	chmod 600 /var/www/html/.ssh/authorized_keys
+if [ -z "$(getent passwd | grep $FTP_USER)" ]; then
+	adduser -D -G ftp -g ftp -h /var/www/html/ $FTP_USER
+	FTP_PASSWORD=$(base64 /run/secrets/ftp_password)
+	echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 	chown root:root /var/www/html/
-	sed -i "s/Match User/Match User $SFTP_USER/g" /etc/ssh/sshd_config
 fi
-exec /usr/sbin/sshd -eD
+exec vsftpd /etc/vsftpd/vsftpd.conf
