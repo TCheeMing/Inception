2573a9ba54cd1f1c763da11c1da9570893c7713e
cteoh <cteoh@alpine.default>
2025-05-07 23:51:28 +0800

    refactor: add comments and clean up commands

--- srcs/requirements/mariadb/tools/mariadb-run.sh
+++ srcs/requirements/mariadb/tools/mariadb-run.sh
@@ -1,28 +1,46 @@
 #!/bin/sh
 
-if [ -z "$(getent passwd | grep $MARIADB_USER)" ]; then
-	adduser -D -G mysql -g mysql $MARIADB_USER
-fi
+# First-time user setup
+# if [ -z "$(getent passwd | grep $MARIADB_USER)" ]; then
+#	adduser -D -G mysql -g mysql $MARIADB_USER
+# fi
+
+# First-time MariaDB setup
 if [ -z "$(ls -A /var/lib/mysql/ 2> /dev/null)" ]; then
 	MARIADB_ROOT_PASSWORD=$(base64 /run/secrets/mariadb_root_password)
 	MARIADB_PASSWORD=$(base64 /run/secrets/mariadb_password)
-	mariadb-install-db --user=$MARIADB_USER --skip-test-db
-	mariadbd-safe --user=$MARIADB_USER --no-watch
+	mariadb-install-db --skip-test-db
+	mariadbd-safe --user=root --no-watch
 	sleep 3
-	MARIDB_PID=$(pgrep mariadb)
-	mariadb -e "UPDATE mysql.global_priv SET priv=json_set(priv, '\$.plugin', 'mysql_native_password', '\$.authentication_string', PASSWORD('$MARIADB_ROOT_PASSWORD')) WHERE User='root';"
-	mariadb -e "RENAME USER '$MARIADB_USER'@'localhost' TO '$MARIADB_USER'@'%';"
-	mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
-	mariadb -e "UPDATE mysql.global_priv SET priv=json_set(priv, '\$.plugin', 'mysql_native_password', '\$.authentication_string', PASSWORD('$MARIADB_PASSWORD')) WHERE User='$MARIADB_USER';"
-	mariadb -e "DELETE FROM mysql.global_priv WHERE User='';"
-	mariadb -e "DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	MARIDB_PID=$(pgrep /usr/bin/mariadbd)
+
+	# Secure installation to improve database security.
+	# Needed especially for < 10.4.
+	# mariadb -e "UPDATE mysql.global_priv SET priv=json_set(priv, '$.password_last_changed', UNIX_TIMESTAMP(), '$.plugin', 'mysql_native_password', '$.authentication_string', 'invalid', '$.auth_or', json_array(json_object(), json_object('plugin', 'unix_socket'))) WHERE User='root';"
+	mariadb -e "UPDATE mysql.global_priv SET priv=json_set(priv, '$.plugin', 'mysql_native_password', '$.authentication_string', PASSWORD('$MARIADB_ROOT_PASSWORD')) WHERE User='root';"
+	# mariadb -e "DELETE FROM mysql.global_priv WHERE User='';"
+	# mariadb -e "DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	# mariadb -e "DROP DATABASE IF EXISTS test;"
+	# mariadb -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
+	
+	# WordPress Database Preparation
+	# https://developer.wordpress.org/advanced-administration/before-install/creating-database/
 	mariadb -e "CREATE DATABASE IF NOT EXISTS $MARIADB_WP_NAME";
+	mariadb -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';"
+	mariadb -e "GRANT ALL PRIVILEGES ON $MARIADB_WP_NAME.* TO '$MARIADB_USER'@'%';"
+	
+	mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
+
+	# Gitea Database Preparation
+	# https://docs.gitea.com/installation/database-prep 
 	mariadb -e "CREATE USER 'gitea'@'%' IDENTIFIED BY 'gitea';"
 	mariadb -e "CREATE DATABASE giteadb CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin';"
 	mariadb -e "GRANT ALL PRIVILEGES ON giteadb.* TO 'gitea';"
+
 	mariadb -e "FLUSH PRIVILEGES;"
+
 	kill -s 15 $MARIDB_PID
-	wait $MARIADB_PID
+	# wait $MARIADB_PID
 	sleep 3
 fi
-exec mariadbd-safe --user=$MARIADB_USER
+exec mariadbd --user=root
