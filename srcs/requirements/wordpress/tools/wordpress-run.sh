063ef4b656a3e562ee655cfe450f5cf71f6480e2
cteoh <cteoh@alpine.default>
2025-05-10 23:52:32 +0800

    refactor: change entire wordpress setup into using wp-cli
    1) Generate wp_config.php using wp-cli instead of copying from host into
       container.
    2) Replace MariaDB connection test script with wp-cli in a while loop.
    
    1) Minimize the usage of "prepared" files and rather let the whole build
       and setup process to generate the necessary files.
    2) Script connection does not seem to reflect the ability for WordPress
       to establish a connectionwith the database (latter would fail even
       though the former succeeds).

--- srcs/requirements/wordpress/tools/wordpress-run.sh
+++ srcs/requirements/wordpress/tools/wordpress-run.sh
@@ -1,18 +1,48 @@
 #!/bin/sh
 
-if [ -z "$(ls -A /var/www/html/ 2> /dev/null)" ]; then
+if ! [ wp core is-installed 2> /dev/null ]; then
 	MARIADB_PASSWORD=$(cat /run/secrets/mariadb_password)
 	WP_ADMIN_PASSWORD=$(cat /run/secrets/wordpress_admin_password)
 	WP_USER_ONE_PASSWORD=$(cat /run/secrets/wordpress_user_one_password)
-	wget --no-verbose https://wordpress.org/wordpress-6.6.2.tar.gz
-	tar --strip-components=1 -xf wordpress-6.6.2.tar.gz
-	rm -rf *tar.gz
-	mv /root/wp-config.php /root/test_page.php /var/www/html/
-	sed -i "s|define( 'DB_NAME', 'database_name_here' );|define( 'DB_NAME', '$MARIADB_WORDPRESS_NAME' );|g" wp-config.php
-	sed -i "s|define( 'DB_USER', 'username_here' );|define( 'DB_USER', '$MARIADB_USER' );|g" wp-config.php
-	sed -i "s|define( 'DB_PASSWORD', 'password_here' );|define( 'DB_PASSWORD', '$MARIADB_PASSWORD' );|g" wp-config.php
-	php /root/db-connection-test.php
-	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email
+	mv /root/wordpress/* /var/www/html/
+	rm -rf /root/wordpress/
+
+	# Creates wp_config.php for installation and populates the WordPress database in MariaDB with necessary tables.
+	wp config create --dbname=$MARIADB_WORDPRESS_NAME \
+			 --dbuser=$MARIADB_USER	\
+			 --dbpass=$MARIADB_PASSWORD \
+			 --dbhost=mariadb:3306 2> /dev/null
+	while [ $? -eq 1 ]
+	do
+		wp config create --dbname=$MARIADB_WORDPRESS_NAME \
+				 --dbuser=$MARIADB_USER	\
+				 --dbpass=$MARIADB_PASSWORD \
+				 --dbhost=mariadb:3306 2> /dev/null
+	done 
+
+	# Sets necessary WordPress constants (especially for Redis connection).
+	wp config set WP_DEBUG true --raw
+	wp config set WP_REDIS_HOST redis
+	wp config set WP_REDIS_PORT 6379
+	wp config set WP_REDIS_PREFIX redis_
+
+	# Installs WordPress and creates an administrator.
+	wp core install --url=$DOMAIN_NAME \
+			--title=$WORDPRESS_TITLE \
+			--admin_user=$WORDPRESS_ADMIN \
+			--admin_password=$WP_ADMIN_PASSWORD \
+			--admin_email=$WORDPRESS_ADMIN_EMAIL \
+			--skip-email 2> /dev/null
+	while [ $? -eq 1 ]
+	do	
+		wp core install --url=$DOMAIN_NAME \
+				--title=$WORDPRESS_TITLE \
+				--admin_user=$WORDPRESS_ADMIN \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WORDPRESS_ADMIN_EMAIL \
+				--skip-email 2> /dev/null
+	done
+
 	wp user create $WORDPRESS_USER_ONE $WORDPRESS_USER_ONE_EMAIL --user_pass=$WP_USER_ONE_PASSWORD
 fi
 exec php-fpm83 --nodaemonize
--- srcs/requirements/wordpress/wordpress-php.Dockerfile
+++ srcs/requirements/wordpress/wordpress-php.Dockerfile
@@ -6,14 +6,15 @@ ENV TZ="Asia/Kuala_Lumpur"
 
 WORKDIR /var/www/html/
 
-RUN apk add --no-cache php83 php83-fpm php83-mysqli php83-phar php83-iconv wget tzdata && \
+RUN apk add --no-cache php83 php83-fpm php83-mysqli php83-phar php83-iconv php83-tokenizer curl tzdata && \
     cp /etc/php83/php-fpm.conf /etc/php83/php-fpm.conf.default && \
     cp /etc/php83/php-fpm.d/www.conf /etc/php83/php-fpm.d/www.conf.default && \
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+    curl --remote-name https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
     chmod +x wp-cli.phar && \
-    mv wp-cli.phar /usr/local/bin/wp
+    mv wp-cli.phar /usr/local/bin/wp && \
+    php -d memory_limit=512M /usr/local/bin/wp core download --path=/root/wordpress/
 
-COPY ./tools/wordpress-run.sh ./tools/db-connection-test.php ./configs/wp-config.php ./tools/test_page.php /root/
+COPY ./tools/wordpress-run.sh ./tools/test_page.php /root/
 COPY ./configs/php-fpm.conf /etc/php83/
 COPY ./configs/www.conf /etc/php83/php-fpm.d/www.conf
 
