e0230cbb9636758f070e1987f7dfebf53353e1cb
cteoh <cteoh@alpine.default>
2025-05-10 13:50:07 +0800

    refactor: remove the use of base64 for secrets

--- srcs/requirements/bonus/ftp/tools/ftp-run.sh
+++ srcs/requirements/bonus/ftp/tools/ftp-run.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$(getent passwd | grep $FTP_USER)" ]; then
 	adduser -D -G ftp -g ftp -h /var/www/html/ $FTP_USER
-	FTP_PASSWORD=$(base64 /run/secrets/ftp_password)
+	FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 	echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 	chown root:root /var/www/html/
 fi
